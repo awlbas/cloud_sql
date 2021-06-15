b3faab819ed945032672131b51a7c390669d274c
awlbas <awladibaskara@gmail.com>
2021-06-15 14:06:00 +0700

    init

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <?php
 
-try{
-    $dbh = new pdo( 'mysql:dbname=laravel;host=127.0.0.1',
-                    'sail',
-                    'password',
-                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    die(json_encode(array('outcome' => true)));
-}
-catch(PDOException $ex){
-    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
-}
+try {
+    # [START cloud_sql_mysql_pdo_create_socket]
+    $username = 'sail';
+    $password = 'password';
+    $dbName = 'laravel';
+    $connectionName = getenv("laravel-316805:asia-southeast2:laravel");
+    $socketDir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
+    array $conn_config;
+
+    // Connect using UNIX sockets
+    $dsn = sprintf(
+        'mysql:dbname=%s;unix_socket=%s/%s',
+        $dbName,
+        $socketDir,
+        $connectionName
+    );
+
+    // Connect to the database.
+    $conn = new PDO($dsn, $username, $password);
+    # [END cloud_sql_mysql_pdo_create_socket]
+} catch (TypeError $e) {
+    throw new RuntimeException(
+        sprintf(
+            'Invalid or missing configuration! Make sure you have set ' .
+            '$username, $password, $dbName, and $dbHost (for TCP mode) ' .
+            'or $connectionName (for UNIX socket mode). ' .
+            'The PHP error was %s',
+            $e->getMessage()
+        ),
+        $e->getCode(),
+        $e
+    );
